2ab38fe30bd13fd506dc654f50ffb99340ce07eb
Josip Harambasic <josip@Josips-MacBook-Pro.local>
2022-05-10 22:43:46 +0200

    update

--- dva_ex3_josip_harambasic.py
+++ dva_ex3_josip_harambasic.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import pandas as pd
 
@@ -24,74 +26,30 @@ DEFAULT_CENTROIDS = np.array([[5.664705882352942, 3.0352941176470587, 3.33529411
                               [5.828571428571429, 2.9357142857142855, 3.664285714285714, 1.1]])
 
 
-selectingTool = Select(title="Random Centroids", value="False", options=list(["True","False"]))
-upper_slider = Slider(start=2, end=10, step=1, value=3, title="k")
-
-colormap = {'setosa': Spectral10[0], 'versicolor': Spectral10[1], 'virginica': Spectral10[2]}
-colors = [colormap[x] for x in flowers['species']]
-
-p = figure(title="Scatterplot of flower distribution by petal length and sepal length", width=450, height=450)
-p.xaxis.axis_label = 'Petal Length'
-p.yaxis.axis_label = 'Sepal length'
-
-p.scatter(flowers["petal_length"], flowers["sepal_length"],
-          color=colors, fill_alpha=0.2, size=10)
-
-#################################################################################
-
-p2 = figure(title="Scatterplot of flower distribution by petal width and petal width",width=450, height=450)
-p2.xaxis.axis_label = 'Petal width'
-p2.yaxis.axis_label = 'Petal length'
-
-p2.scatter(flowers["petal_width"], flowers["petal_length"],
-          color=colors, fill_alpha=0.2, size=10)
-
-#################################################################################
-selectingTool2 = Select(title="Random Centroids", value="False", options=list(["True","False"]))
-upper_slider2 = Slider(start=2, end=10, step=1, value=3, title="k")
-
-colormap = {'setosa': Spectral10[0], 'versicolor': Spectral10[1], 'virginica': Spectral10[2]}
-colors = [colormap[x] for x in flowers['species']]
-
-p3 = figure(title="Scatterplot of flower distribution by petal length and sepal length", width=450, height=450)
-p3.xaxis.axis_label = 'Petal Length'
-p3.yaxis.axis_label = 'Sepal length'
-
-p3.scatter(flowers["petal_length"], flowers["sepal_length"],
-          color=colors, fill_alpha=0.2, size=10)
-
-#################################################################################
-
-p4 = figure(title="Scatterplot of flower distribution by petal width and petal width",width=450, height=450)
-p4.xaxis.axis_label = 'Petal width'
-p4.yaxis.axis_label = 'Petal length'
-
-p4.scatter(flowers["petal_width"], flowers["petal_length"],
-          color=colors, fill_alpha=0.2, size=10)
-
-lt = layout(
-    column(row(column(selectingTool,upper_slider),p,p2), row(column(selectingTool2,upper_slider2),p3,p4))
-)
-
-curdoc().add_root(lt)
-curdoc().title = "DVA_ex_3"
-
-#show(p)
-
-
 def get_closest(data_point: np.ndarray, centroids: np.ndarray):
     """
     Takes a data_point and a nd.array of multiple centroids and returns the index of the centroid closest to data_point
     by computing the euclidean distance for each centroid and picking the closest.
-
-    :param data_point:
-    :param centroids:
-    :return:
     """
-    return # the index of the centroid closest to the datapoint
+    N = centroids.shape[0]
+    dist = np.empty(N)
+    for i, c in enumerate(centroids):
+        dist[i] = np.linalg.norm(c - data_point)
+    index_min = np.argmin(dist)
+    return index_min
 
 
-def k_means(data: np.ndarray, k:int=3, n_iter:int=500, random_initialization=False) -> Tuple[np.ndarray, int]:
+def to_classes(clustering):
+    # Get number of samples (you can pass it directly to the function)
+    num_samples = sum(x.shape[0] for x in clustering)
+    indices = np.empty((num_samples,))  # An empty array with correct size
+    for ith, cluster in enumerate(clustering):
+        # use cluster indices to assign to correct the cluster index
+        indices[cluster] = ith
+    return indices.astype(int)
+
+
+def k_means(data: np.ndarray, k: int = 3, n_iter: int = 500, random_initialization=False) -> Tuple[np.ndarray, int]:
     """
     Your k-means implementation. (of course, no other library can be used for this) of course checking the internet
     for other implementations is a good way to start!
@@ -105,7 +63,12 @@ def k_means(data: np.ndarray, k:int=3, n_iter:int=500, random_initialization=Fal
                       n_iterations: the number of iterations it took until the algorithm terminated)
     """
     # Initialize the algorithm by assigning random cluster labels to each entry in your dataset
-    clustering = ...
+    k = k + 1
+    centroids = data[random.sample(range(len(data)), k)]
+    labels = np.array([np.argmin([(el - c) ** 2 for c in centroids]) for el in data])
+    clustering = []
+    for k in range(k):
+        clustering.append(data[labels == k])
 
     # Implement K-Means with a while loop, which terminates either if the centroids don't move anymore, or
     # if the number of iterations exceeds n_iter
@@ -113,27 +76,72 @@ def k_means(data: np.ndarray, k:int=3, n_iter:int=500, random_initialization=Fal
     while counter < n_iter:
         # Compute the new centroids, if random_initialization is false use DEFAULT_CENTROIDS in the first iteration
         # if you use DEFAULT_CENTROIDS, make sure to only pick the k first entries from them.
-
+        if random_initialization is False and counter == 0:
+            centroids = DEFAULT_CENTROIDS[random.sample(range(len(DEFAULT_CENTROIDS)), k)]
 
         # Update the cluster labels using get_closest
+        labels = np.array([get_closest(el, centroids) for el in data])
+        clustering = []
+        for i in range(k):
+            clustering.append(np.where(labels == i)[0])
 
+        counter += 1
+
+        new_centroids = np.zeros_like(centroids)
+        for i in range(k):
+            if len(clustering[i]) > 0:
+                new_centroids[i] = data[clustering[i]].mean(axis=0)
+            else:
+                new_centroids[i] = centroids[i]
 
         # if the centroids didn't move, exit the while loop
+        if clustering is not None and (centroids != new_centroids).sum() == 0:
+            break
+        else:
+            centroids = new_centroids
         pass
 
     # return the final cluster labels and the number of iterations it took
+    clustering = to_classes(clustering)
     return clustering, counter
 
 
 def callback(attr, old, new):
     # recompute the clustering and update the colors of the data points based on the result
-    pass
+    k = slider_k.value_throttled
+    init = select_init.value
+    clustering_new, counter_new = k_means(data_np, k, 500, init)
+
+    source.data['clustering'] = clustering_new.astype(str)
+    mapper = factor_cmap('clustering', palette=Spectral10, factors=np.unique(clustering_new).astype(str))
+    scatter1.glyph.fill_color = mapper
+    scatter2.glyph.fill_color = mapper
+    scatter1.glyph.line_color = mapper
+    scatter2.glyph.line_color = mapper
+    div.text = 'Number of iterations: %d' % (counter_new)
+
 
 # read and store the dataset
-#data: pd.DataFrame = flowers.copy(deep=True)
-#data = data.drop(['species'], axis=1)
+data: pd.DataFrame = flowers.copy(deep=True)
+data = data.drop(['species'], axis=1)
 
 # Create a copy of the data as numpy array, which you can use for computing the clustering
+data_np = np.asarray(data)
+
+# Create the dashboard
+# 1. A Select widget to choose between random initialization or using the DEFAULT_CENTROIDS on top
+select_init = Select(title='Random Centroids',value='False',options=['True','False'])
+
+# 2. A Slider to choose a k between 2 and 10 (k being the number of clusters)
+slider_k = Slider(start=2,end=10,value=3,step=1,title='k')
+
+# 4. Connect both widgets to the callback
+select_init.on_change('value',callback)
+slider_k.on_change('value_throttled',callback)
+
+# 3. A ColumnDataSource to hold the data and the color of each point you need
+clustering, counter = k_means(data_np,4,500,False)
+source = ColumnDataSource(dict(petal_length=data['petal_length'],sepal_length=data['sepal_length'],petal_width=data['petal_width'],clustering=clustering))
 
 # Create the dashboard
 # 1. A Select widget to choose between random initialization or using the DEFAULT_CENTROIDS on top
@@ -149,13 +157,35 @@ def callback(attr, old, new):
 #
 # Use a categorical color mapping, such as Spectral10, have a look at this section of the bokeh docs:
 # https://docs.bokeh.org/en/latest/docs/user_guide/categorical.html#filling
+mapper = factor_cmap('clustering',palette=Spectral10,factors=np.unique(clustering).astype(str))
+
+plot1 = figure(title='Scatterplot of flowers distribution by petal length and sepal length')
+plot1.yaxis.axis_label = 'Sepal length'
+plot1.xaxis.axis_label = 'Petal length'
+scatter1 = plot1.scatter(x='petal_length',
+                         y='sepal_length',
+                         fill_alpha=0.4,
+                         source=source,
+                         fill_color=mapper,
+                         line_color=mapper)
+
+plot2 = figure(title='Scatterplot of flowers distribution by petal width and petal length')
+plot2.yaxis.axis_label = 'Petal length'
+plot2.xaxis.axis_label = 'Petal width'
+scatter2 = plot2.scatter(x='petal_width',
+                         y='petal_length',
+                         fill_alpha=0.4,
+                         source=source,
+                         fill_color=mapper,
+                         line_color=mapper)
 # 5. A Div displaying the currently number of iterations it took the algorithm to update the plot.
+div = Div(text='Number of iterations: ')
 
-#lt = row(...your plot layout)
 
-#curdoc().add_root(lt)
-#curdoc().title = "DVA_ex_3"
+lt = row(column(select_init,slider_k,div),plot1,plot2)
 
+curdoc().add_root(lt)
+curdoc().title = "DVA_ex_3"
 
 # Run it with
 # bokeh serve --show dva_ex3_yourname.py
